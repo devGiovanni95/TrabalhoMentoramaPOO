5adfc5759a9003e61fab9c65707453407bf67686
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-06-22 19:39:53 -0300

    implementando

--- src/MostraConta.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.Consumer;
-
-public class MostraConta implements Consumer {
-
-    @Override
-    public void accept(Object o) {
-//        System.out.println(o);
-    }
-}
